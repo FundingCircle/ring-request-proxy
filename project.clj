2902f73efd3b545db29f92294304cb4fd6e58cf1
Sasha Gerrand <sasha.gerrand@fundingcircle.com>
2017-10-16 18:06:20 +0200

    Version 0.1.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ring-request-proxy "0.1.4"
+(defproject ring-request-proxy "0.1.5"
   :description "Ring request proxy"
   :url "https://github.com/FundingCircle/ring-request-proxy"
   :license {:name "BSD 3-clause"
